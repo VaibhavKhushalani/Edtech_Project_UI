c4bcff90889c7ff9e6c0e4bafd712bb1752f3790
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2022-12-31 02:22:26 +0530

    code cleaning

--- src/Component/Common/NavBar.jsx
+++ src/Component/Common/NavBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useNavigate } from "react-router-dom";
 const NavBar = ({ setSelect }) => {
   const Role = localStorage.getItem("role");
   return (
